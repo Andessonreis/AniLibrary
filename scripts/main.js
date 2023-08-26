2aa485f3490c98dbcd8b84cb3eb0e86ff959f82e
andesson reis <andessonreys@gmail.com>
2023-08-26 15:38:26 -0300

    feat: add rodape

--- scripts/main.js
+++ scripts/main.js
@@ -60,8 +60,6 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-
-
 // banner
 document.addEventListener("DOMContentLoaded", function() {
     const carousel = document.querySelector('.carousel');
@@ -85,7 +83,6 @@ document.addEventListener("DOMContentLoaded", function() {
     showBanner(currentIndex);
     setInterval(nextBanner, 5000); // Alterne os banners a cada 5 segundos
 });
-
 
 
 // Catálogo de animes
@@ -114,7 +111,7 @@ const animeList = document.querySelector('.anime-list');
 function displayAnimeCatalog() {
     let catalogHTML = '';
     for (const anime of animeCatalog) {
-        for (let i = 0; i < 7; i++) {
+        for (let i = 0; i < 5; i++) {
             catalogHTML += anime.generateHTML();
         }
     }
